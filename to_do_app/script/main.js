147055b2a750b61d647c701610423531e2c9358a
Nk <nazmulkabir512@gmail.com>
2018-11-30 13:13:37 +0600

    added bootstrap for to=do-app

--- to_do_app/script/main.js
+++ to_do_app/script/main.js
@@ -18,15 +18,55 @@ function addItem(){
         input.value = "";
     }
     else{
-        document.querySelector(".msg").innerHTML = "NO item to add";
+        var msg = document.querySelector(".msg");
+        msg.classList.add("alert");
+        msg.classList.add("alert-warning");
+        msg.innerHTML = "NO item to add";
     }
 }
 function clearMsg(){
-    document.querySelector(".msg").innerHTML = "";
+    var msg = document.querySelector(".msg");
+    msg.innerHTML = "";
 }
 function clearUL(){
     document.querySelector(".listView").innerHTML = "";
 }
 function attachItem(){
-// have to write;
+    var ul = document.querySelector(".listView");
+    for(var i=0 ; i<itemList.length ; i++){
+        var li = makeListItem(itemList[i]);
+        ul.appendChild(li);
+    }
 }
+function makeListItem(text){
+  var li = document.createElement("li");
+  li.classList.add("list-group-item");
+  var button = document.createElement("button");
+  button.classList.add("btn");
+  button.classList.add("btn-outline-secondary");
+
+  button.innerHTML = "x";
+  button.onclick = function (){
+      clearMsg();
+      deleteItem(this);
+  }
+
+  var textnode = document.createTextNode(text);
+  
+  li.appendChild(button);
+  li.appendChild(textnode);
+  return li;
+
+}
+
+function deleteItem(button){
+    var item = button.parentNode.lastChild.textContent;
+    var index = itemList.indexOf(item);
+    if(index !== -1){
+        itemList.splice(index,1);
+        clearUL();
+        attachItem();
+    }
+}
+
+attachItem();
